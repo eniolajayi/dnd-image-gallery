29e0d008648c72cda9a9b6e59c49254ecdbf0584
eniolajayi <ajayieniola16@gmail.com>
2023-09-20 15:01:12 +0100

    show gallery link after login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,33 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { getSession } from "@auth0/nextjs-auth0";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getSession();
   return (
     <main className="w-full h-screen flex items-center gap-5 flex-col justify-center">
       <div>
         <h1 className="text-4xl font-medium">Welcome to Budget Museum</h1>
       </div>
+
       <div className="">
         <h2 className="text-xl text-slate-600 capitalize">
           Sign In to View Gallery
         </h2>
       </div>
       <div>
-        <a className="underline text-semibold text-blue-600" href="/api/auth/login">
-          Login
-        </a>
+        {session ? (
+          <Button>
+            <Link href={"/gallery"}>View Gallery</Link>
+          </Button>
+        ) : (
+          <a
+            className="underline text-semibold text-blue-600"
+            href="/api/auth/login"
+          >
+            Login
+          </a>
+        )}
       </div>
     </main>
   );
