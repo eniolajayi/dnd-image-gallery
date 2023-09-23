d745105d70b64b3bbdbf93034fa270b8f99f093b
eniolajayi <ajayieniola16@gmail.com>
2023-09-23 10:40:10 +0100

    make link easy to click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,21 @@ export default async function Home() {
   return (
     <main className="w-full h-screen flex items-center gap-5 flex-col justify-center">
       <div>
-        <h1 className="text-4xl font-medium text-center">Welcome to Budget Museum</h1>
+        <h1 className="text-4xl font-medium text-center">
+          Welcome to Budget Museum
+        </h1>
       </div>
 
       <div className="">
         <h2 className="text-xl text-slate-600 capitalize">
-          Sign In to View Gallery
+          {session ? "" : "Sign In to View Gallery"}
         </h2>
       </div>
       <div>
         {session ? (
-          <Button>
-            <Link href={"/gallery"}>View Gallery</Link>
-          </Button>
+          <Link className="underline" href={"/gallery"}>
+            View Gallery
+          </Link>
         ) : (
           <a
             className="underline text-semibold text-blue-600"
